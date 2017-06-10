b5a53ab66d2ffcd59d55036e5cfb12d6ef49c913
Priyanka <priyanka.khire@gmail.com>
2017-06-10 00:17:20 -0700

    Basic tree with all traversals

---
+++ BasicTree.py
@@ -0,0 +1,81 @@
+#Tree data structure and traversals
+
+class node:
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+
+    def setData(self, data):
+        self.data = data
+
+    def setLeft(self, left):
+        self.left = left
+
+    def setRight(self, right):
+        self.right = right
+
+    def getData(self):
+        return self.data
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+
+def createNode(data):
+    newNode = node()
+    newNode.setData(data)
+    return newNode
+
+def addLeft(data, root):
+    lnode = createNode(data)
+    root.setLeft(lnode)
+    return lnode
+
+def addRight(data, root):
+    rnode = createNode(data)
+    root.setRight(rnode)
+    return rnode
+
+def inorder(root):
+    if root == None:
+        return
+    #Go left
+    inorder(root.getLeft())
+    print root.getData()
+    #Go right
+    inorder(root.getRight())
+
+def postorder(root):
+    if root == None:
+        return
+    #Left
+    postorder(root.getLeft())
+    #Right
+    postorder(root.getRight())
+    print root.getData()
+
+def preorder(root):
+    if root == None:
+        return
+    print root.getData()
+    #Left
+    preorder(root.getLeft())
+    #Right
+    preorder(root.getRight())
+
+#Main Program
+root = createNode(1)
+node1= addLeft(2, root)
+node2 = addRight(3, root)
+node3 = addLeft(4, node1)
+node4 = addRight(5, node1)
+node5 = addLeft(6, node2)
+node6 = addRight(7, node2)
+inorder(root)
+print "\n\n"
+postorder(root)
+print "\n\n"
+preorder(root)
