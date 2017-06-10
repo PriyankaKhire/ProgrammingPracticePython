e2fde2bd13a8184f92689d2bbb285d8b673dc1b9
Priyanka <priyanka.khire@gmail.com>
2017-06-10 12:16:14 -0700

    Modified the tree library

--- Tree.py
+++ Tree.py
@@ -23,19 +23,18 @@ class node:
     def getRight(self):
         return self.right
 
-class LinkedList:
-    def createNode(data):
-        newNode = node()
-        newNode.setData(data)
-        return newNode
+def createNode(data):
+    newNode = node()
+    newNode.setData(data)
+    return newNode
 
-    def addLeft(data, root):
-        lnode = createNode(data)
-        root.setLeft(lnode)
-        return lnode
+def addLeft(data, root):
+    lnode = createNode(data)
+    root.setLeft(lnode)
+    return lnode
 
-    def addRight(data, root):
-        rnode = createNode(data)
-        root.setRight(rnode)
-        return rnode
+def addRight(data, root):
+    rnode = createNode(data)
+    root.setRight(rnode)
+    return rnode
     
