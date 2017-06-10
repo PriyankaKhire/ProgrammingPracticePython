498f28458f7fbec55d384f0adf5229758f94a9f3
Priyanka <priyanka.khire@gmail.com>
2017-06-10 12:39:08 -0700

    Working program wont print last node

--- TreeToDLL.py
+++ TreeToDLL.py
@@ -4,6 +4,7 @@ from Tree import createNode
 from Tree import addLeft
 from Tree import addRight
 
+#Bad implementation, lesson learnt, need golbal prev variable and not local within function
 head = node()
 def foo(root, prev):
     global head
@@ -25,6 +26,22 @@ def foo(root, prev):
         prev.setLeft(root) 
     foo(root.getRight(), root)
 
+prev = node()
+def inorder(root):
+    global head
+    global prev
+    if root == None:
+        return
+    inorder(root.getLeft())
+    if prev.getData() == None:
+        print "root is "+str(root.getData())+" prev is none"
+        head = root
+    else:
+        print "root is "+str(root.getData())+" prev is "+str(prev.getData())
+        prev.setRight(root)
+        root.setLeft(prev)
+    prev = root
+    inorder(root.getRight())
 
 #Main
 root = createNode(1)
@@ -34,5 +51,11 @@ node3 = addLeft(4, node1)
 node4 = addRight(5, node1)
 node5 = addLeft(6, node2)
 node6 = addRight(7, node2)
-#Cannot create this inplace
-foo(root, head)
+#foo(root, head)
+inorder(root)
+#Print dll.
+ptr= node()
+ptr = head
+while ptr.getRight() :
+    print ptr.getData()
+    ptr = ptr.getRight()
