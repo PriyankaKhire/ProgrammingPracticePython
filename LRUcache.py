093445e07f480626726bd0d50817dcfd93aa374a
Priyanka Khire <priyanka.khire@gmail.com>
2018-03-28 13:00:54 -0700

    Complete working implementation

--- LRUcache.py
+++ LRUcache.py
@@ -1,4 +1,6 @@
 from multiprocessing import Queue
+#Doubly Linked List queue
+from multiprocessing import Queue
 class Node(object):
     def __init__(self):
         self.data = None
@@ -35,18 +37,10 @@ class DLLqueue(object):
     def createNode(self, data):
         newNode = Node()
         newNode.setData(data)
-        #if head is null then assign it
-        if(not self.headNode):
-            self.headNode = newNode
-        if(not self.tailNode):
-            self.tailNode = newNode
-        print newNode
-        print newNode.getNext
-        print newNode.getPrev
         return newNode
 
     def isEmpty(self):
-        if (self.headNode == None):
+        if (self.elementCount == 0):
             return True
         return False
 
@@ -63,18 +57,58 @@ class DLLqueue(object):
             return False
         #Create new node for queue
         node = self.createNode(data)
-        #Add it from head of the queue
-        node.setNext(self.headNode)
-        self.headNode.setPrev(node)
-        #update head of the queue
-        self.headNode = node
+        #Check if its the first node to be added
+        if(self.headNode == None and self.tailNode == None):
+            self.headNode = node
+            self.tailNode = node
+        else:
+            #Add node from front
+            node.setNext(self.headNode)
+            self.headNode.setPrev(node)
+            #Update head node
+            self.headNode = node
         #update the element count
         self.elementCount = self.elementCount + 1
-        
+        return node
 
-#######################
-        #Error in display and push function of queue, to check
-        #########################
+    #Pops element from tail of the queue
+    def pop(self):
+        if(self.isEmpty()):
+            print "Cannot pop, queue is empty"
+            return False
+        #Stores the recently popped
+        popped = self.tailNode.getData()
+        #Check if it's the last node
+        if(self.headNode == self.tailNode):
+            self.headNode = None
+            self.tailNode = None
+        else:
+            #Pop the node from tail
+            prev = self.tailNode.getPrev()
+            prev.setNext(None)
+            self.tailNode.setPrev(None)
+            #Update prev node.
+            self.tailNode = prev
+        #Update element count
+        self.elementCount = self.elementCount - 1
+        #Return the recently popped item
+        return popped
+
+    #This function takes the node and puts it in front of the queue
+    def updateQ(self, node):
+        if(node == self.headNode):
+            return
+        prev = node.getPrev()
+        nxt = node.getNext()
+        if(prev != None):
+            prev.setNext(nxt)
+        if(nxt != None):
+            nxt.setPrev(prev)
+        node.setPrev(None)
+        node.setNext(self.headNode)
+        self.headNode = node
+        self.display()
+
     def display(self):
         ptr = Node()
         ptr = self.headNode
@@ -82,69 +116,59 @@ class DLLqueue(object):
             print ptr.getData()
             print "\n"
             ptr = ptr.getNext()
-            
-        
 
 
 class LRUCache(object):
 
     def __init__(self, capacity):
         self.hash_table = {}
-        self.q = Queue(maxsize=capacity)
-        print self.q
+        self.q = DLLqueue(capacity)
         
     #Function returns true if LRU cache is full
     def isFull(self):
-        if(self.q.full()):
+        if(self.q.isFull()):
             return True
         return False
         
     #Function returns true if LRU cache is empty
     def isEmpty(self):
-        if(self.q.empty()):
+        if(self.q.isEmpty()):
             return True
         return False
 
     def get(self, key):
         #Cannot get anything if cache is empty
         if(self.isEmpty()):
-            return -1
-        
+            return -1       
         #Get element from cache
         if key in self.hash_table:
-            return self.hash_table[key]
+            #Update queue
+            qNode = self.hash_table[key][1]
+            self.q.updateQ(qNode)
+            return self.hash_table[key][0]
         return -1
-        """
-        :type key: int
-        :rtype: int
-        """
         
 
     def put(self, key, value):
         if(self.isFull()):
             #Remove least recently used item
-            least_recently_used_item = self.q.get
+            least_recently_used_item = self.q.pop()
             self.hash_table.pop(least_recently_used_item, None)
-            print "Removed "+str(least_recently_used_item)
-        
+            print "Removed "+str(least_recently_used_item)        
         #Put element in hash table
-        self.hash_table[key] = value
-        self.q.put(key)
+        node_address = self.q.push(key)
+        self.hash_table[key] = [value, node_address]
         
-        """
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
+        
         
 
 
 #inputs
-cache = LRUCache(2)
-cache.put(1, 1)
-cache.put(2, 2)
-print cache.get(1);  
-q = DLLqueue(4)
-q.push(1)
-q.push(2)
-
+cache = LRUCache(3)
+cache.put(1, "hi")
+cache.put(2, "bye")
+cache.put(3, "ho")
+cache.put(4, "no")
+cache.q.display()
+print "---"
+print cache.get(2)
