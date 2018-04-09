adc87b9dc3fe239e083678fc16a8e7edf3f1e89b
Priyanka Khire <priyanka.khire@gmail.com>
2018-04-09 12:48:22 -0700

    Minor modifications

--- WordLadder.py
+++ WordLadder.py
@@ -18,9 +18,11 @@ class Solution(object):
         return False
     
     def ladder(self, output, endWord, wordList):
+        print output
+        print "---"
         if(output[-1] == endWord):
-            #print str(len(output))
-            #print output
+            print str(len(output))
+            print output
             if( not self.sol):
                 self.sol=len(output)
             if((self.sol) > len(output)):
@@ -37,6 +39,12 @@ class Solution(object):
             
     
     def ladderLength(self, beginWord, endWord, wordList):
+        if(beginWord not in wordList):
+            return 0
+        if(endWord not in wordList):
+            return 0
+        if(self.differentByOne(beginWord, endWord)):
+            return 2
         output = []
         output.append(beginWord)
         self.ladder(output, endWord, wordList)
@@ -45,4 +53,5 @@ class Solution(object):
 
 #main program
 s = Solution()
-print s.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"])
+#print s.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"])
+print s.ladderLength("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"])
