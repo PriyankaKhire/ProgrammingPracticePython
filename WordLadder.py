7f394a78db0f5e83a8d32e0897a60bea6eab8b41
Priyanka Khire <priyanka.khire@gmail.com>
2018-05-14 12:04:07 -0700

    Added bfs apporach, need to tweak graph create

--- WordLadder.py
+++ WordLadder.py
@@ -16,6 +16,8 @@ class Solution(object):
         if(count == 1):
             return True
         return False
+
+class dfs(Solution):
     
     def ladder(self, output, endWord, wordList):
         print output
@@ -50,8 +52,59 @@ class Solution(object):
         self.ladder(output, endWord, wordList)
         return self.sol
 
+#In this approach we build a graph and then perform dfs or bfs on it
+class graphNode(object):
+    def __init__(self, data, parent):
+        self.data = data
+        self.childern = []
+        self.parent = []
+        self.parent.append(parent)
+        
+class bfs(Solution):
+
+    def createNode(self, data, parent):
+        node = graphNode(data, parent)
+        return node
+
+    def addChildToNode(self, node, child):
+        node.childern.append(child)
+        return node
+
+    def createGraph(self, startWord, inputArray):
+        hash_table = {}
+        q = []
+        root = self.createNode(startWord, None)
+        hash_table[startWord] = root
+        q.append(root)
+        #While q is not empty
+        while q:
+            #Find all words in input array that differ by top element of q by one
+            top = q.pop(0)
+            for word in inputArray:
+                if(self.differentByOne(top.data, word)):
+                    #if word has not been added to graph
+                    if(word not in hash_table):
+                        node = self.createNode(word, top)
+                        top = self.addChildToNode(top, node)
+                        hash_table[word] = node
+                        q.append(node)
+                        print "Added wordssss "+str(word)+" as a child of "+str(top.data)
+                    else:
+                        node = hash_table[word]
+                        #print node.parent.data, node.data, word, top.data, top.parent.data
+                        if(node not in top.parent):
+                            top = self.addChildToNode(top, node)
+                            node.parent.append(top)
+                            print "Added word "+word+" as a child of "+top.data
+                        
+        
+
+    
 
 #main program
-s = Solution()
+s = dfs()
 #print s.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"])
-print s.ladderLength("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"])
+#print s.ladderLength("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"])
+
+b = bfs()
+b.createGraph("hit", ["hot","dot","dog","lot","log","cog"])
