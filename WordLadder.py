aca0f44a82b52a8128532f90f254f5d98326486e
priyankakhire <priyanka.khire@gmail.com>
2018-05-15 15:26:54 -0700

    Added working bfs method where 1st create graph then traverse it

--- WordLadder.py
+++ WordLadder.py
@@ -62,6 +62,16 @@ class graphNode(object):
         
 class bfs(Solution):
 
+    def __init__(self, startWord, endWord, inputArray):
+        self.startWord = startWord
+        self.endWord = endWord
+        self.inputArray = inputArray
+        self.root = None
+
+    def logic(self):
+        self.createGraph()
+        self.graphTraversal_dfs(self.root, [self.root.data])
+
     def createNode(self, data, parent):
         node = graphNode(data, parent)
         return node
@@ -70,34 +80,50 @@ class bfs(Solution):
         node.childern.append(child)
         return node
 
-    def createGraph(self, startWord, inputArray):
+    def graphTraversal_dfs(self, root, path):
+            if(root.data == self.endWord):
+                print path
+                return
+            #for all the childern of the current node
+            for child_node in root.childern:
+                if not child_node.data in path:
+                    path.append(child_node.data)
+                    self.graphTraversal_dfs(child_node, path)
+                    path.pop()
+            
+
+    def createGraph(self):
         hash_table = {}
         q = []
-        root = self.createNode(startWord, None)
-        hash_table[startWord] = root
-        q.append(root)
+        self.root = self.createNode(self.startWord, None)
+        hash_table[self.startWord] = self.root
+        q.append(self.root)
         #While q is not empty
         while q:
             #Find all words in input array that differ by top element of q by one
             top = q.pop(0)
-            for word in inputArray:
+            print "Current top word is "+top.data
+            for word in self.inputArray:
                 if(self.differentByOne(top.data, word)):
                     #if word has not been added to graph
                     if(word not in hash_table):
                         node = self.createNode(word, top)
                         top = self.addChildToNode(top, node)
+                        #We create a double edge
+                        #meaning, parent can also be child of a node
+                        node = self.addChildToNode(node, top)
+                        top.parent.append(node)
                         hash_table[word] = node
                         q.append(node)
-                        print "Added wordssss "+str(word)+" as a child of "+str(top.data)
+                        print "Added wordssss "+str(word)+" as a child of "+str(top.data)+" and vice versa"
                     else:
                         node = hash_table[word]
-                        #print node.parent.data, node.data, word, top.data, top.parent.data
                         if(node not in top.parent):
                             top = self.addChildToNode(top, node)
                             node.parent.append(top)
-                            print "Added word "+word+" as a child of "+top.data
-                        
-        
+                            node = self.addChildToNode(node, top)
+                            top.parent.append(node)
+                            print "Added word "+word+" as a child of "+top.data+" and vice versa"
 
     
 
@@ -106,5 +132,6 @@ s = dfs()
 #print s.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"])
 #print s.ladderLength("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"])
 
-b = bfs()
-b.createGraph("hit", ["hot","dot","dog","lot","log","cog"])
+#b = bfs("hit", "cog", ["hot","dot","dog","lot","log","cog"])
+b = bfs("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"])
+b.logic()
