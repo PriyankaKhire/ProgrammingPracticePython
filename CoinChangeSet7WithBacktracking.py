1813535e300e4a0f69d64c9a18dfbad72a0a1634
Priyanka Khire <priyanka.khire@gmail.com>
2018-06-05 14:17:18 -0700

    Added second non working approach

--- CoinChangeSet7WithBacktracking.py
+++ CoinChangeSet7WithBacktracking.py
@@ -6,19 +6,34 @@ class coinChange(object):
         self.N = N
         self.S = S
 
-    def logic(self, N, S, n, output):
+    def logic_approach2(self, N, S, n, output):
+        if n == N:
+            print output
+            return
+        for i in range(len(S)):
+            if(S[i]+n <=N):
+                output.append(S[i])
+                self.logic_approach2(N, S, S[i]+n, output)
+                #Backtrack
+                output.pop()
+            #Backtrack
+            S.pop()
+
+    def logic_approach1(self, N, S, n, output):
         if n == N:
             print output
             return
         for i in S:
             if(i+n <= N):
                 output.append(i)
-                self.logic(N, S, i+n, output)
+                self.logic_approach1(N, S, i+n, output)
                 #Backtrack
                 output.pop()
 
     def solution(self):
-        self.logic(self.N, self.S, 0, [])
+        self.logic_approach1(self.N, self.S, 0, [])
+        print "-----------"
+        self.logic_approach2(self.N, self.S, 0, [])
 
 #Main Program
 cc = coinChange(10, [2, 5, 3, 6])
