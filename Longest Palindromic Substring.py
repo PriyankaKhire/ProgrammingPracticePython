8a4b307e70a822e5cf0ede9cc030da2793c21c3c
Priyanka Khire <priyanka.khire@gmail.com>
2018-06-19 11:18:52 -0700

    Added working functionality for odd length

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -9,29 +9,35 @@ class Solution(object):
         for char in s:
             new_s = new_s+"$"+char
         new_s = new_s+"$"
-        self.longestPalindromeOddLength(new_s)
+        print new_s
+        palindrome = self.longestPalindromeOddLength(new_s)
+        #remove $ from the string
+        modified_palindrome = ""
+        for letter in palindrome:
+            if letter != "$":
+                modified_palindrome = modified_palindrome + letter
+        print "Palindrome without $ is "+modified_palindrome
+            
 
     def longestPalindromeOddLength(self, s):
         longest_palindrome = ""
         for i in range(1, len(s)):
-            print "i is "+str(i)
             j = 1
             while ((i-j)>=0 and (i+j) < len(s) and s[i-j] == s[i+j]):
                 j = j+1
             if(j != 1):
                 #Palindrome found is between i-j-1 to i+j-1
-                temp_palindrome = s[(j-1-i):(i+j)]
-                print j
-                print j-1-i
-                print i+j
+                temp_palindrome = s[(i-(j-1)):(i+j)]
                 print "temp palindrome is "+temp_palindrome
                 if len(temp_palindrome) > len(longest_palindrome):
                     longest_palindrome = temp_palindrome
                     print "longest palindrome now is "+longest_palindrome
         print "longest palindrome of odd length is "+longest_palindrome
+        return longest_palindrome
             
 
 
 #Mian Program
 o = Solution()
 o.longestPalindromeOfEvenOrOddLength("cbbd")
+o.longestPalindromeOfEvenOrOddLength("babad")
