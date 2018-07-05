cd0cf38e56f2749e82ea62128f813fd0b68786e0
Priyanka Khire <priyanka.khire@gmail.com>
2018-07-05 14:47:06 -0700

    Added method to decode

--- MorseCode.py
+++ MorseCode.py
@@ -11,9 +11,29 @@ class MorseCode(object):
             morse = morse+self.alphabet[char]
         print morse
 
+    def decode_logic(self, morse, currIndex, output):
+        if currIndex == len(morse):
+            print output
+            return
+        #We add 1 to 4 to index to see if it forms morse character or not
+        for i in range(1, 5):
+            if(currIndex+i > len(morse)):
+                break
+            morseCharacter = morse[currIndex:currIndex+i]
+            if(morseCharacter in self.morse):
+                self.decode_logic(morse, currIndex+i, output + self.morse[morseCharacter])
+
+            
+                
+
+                
     def decode(self, morse):
+        #The min number of characters used in morse code is 1 and max is 4
+        self.decode_logic(morse, 0, "")
+        
         
 
 #Main
 o = MorseCode()
-o.encode("geeksforgeeks")
+o.encode("an")
+o.decode(".--.")
