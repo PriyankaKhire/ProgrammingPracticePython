021ce895a40635c45c8bd37fab96475ec95cab80
priyankakhire <priyanka.khire@gmail.com>
2018-07-22 16:53:47 -0700

    Works for most outputs, need to correct for following input

--- Maximum Swap.py
+++ Maximum Swap.py
@@ -48,8 +48,69 @@ class Approch1(object):
         print self.totalNumbers
         print self.zeroToNine
         
+class Approch2(object):
+    def __init__(self, number):
+        self.number = number
+        self.power = 0
 
+    def findPower(self):
+        for i in range(1,9):
+            if not (10**i < self.number):
+                break
+        self.power = i-1
+
+    def findMax(self, number):
+        if number == 0:
+            return 0
+        maxPos = 0
+        maxNum = 0
+        pos = 0
+        while number >0:
+            if(maxNum < (number%10)):
+                maxNum = number%10
+                maxPos = pos
+            number = number/10
+            pos = pos+1
+        return maxNum, maxPos
+                
+    def formulateNumber(self, num, power):
+        formulatedNumber = num*(10**power)
+        for i in range(power):
+            formulatedNumber = formulatedNumber + (num*(10**i))
+        return formulatedNumber
+
+    def numSwap(self, pos1, pos2):
+        stringNum = str(self.number)[::-1]
+        t = list(stringNum)
+        letter1 = t[pos1]
+        letter2 = t[pos2]
+        t[pos2] = letter1
+        t[pos1] = letter2
+        stringNum = ''.join(t)
+        stringNum = stringNum[::-1]
+        return int(stringNum)
+        
+
+    def solution(self):
+        self.findPower()
+        if(self.power < 1):
+            print "No swap"
+            return
+        number = self.number
+        while(self.power >= 1):
+            currNum = number/(10**self.power)
+            compareNumber = self.formulateNumber(currNum, self.power)
+            if(compareNumber < number):
+                #find max in remaining numbers and swap
+                num, pos = self.findMax(number)
+                print "We need to swap "+str(num)+" at postion "+str(pos)+" with number "+str(currNum)+" at position "+str(self.power)
+                output = self.numSwap(pos, self.power)
+                return output
+            number = number%(10**self.power)
+            self.power = self.power-1
+        return "No swap"
+            
 
 #Main Program
-o = Approch1(9793)
-o.solution()
+o = Approch2(10909091)
+print o.solution()
