9bd9ebb579352c9e4dbf93a6ad4f6a1d8134904c
priyankakhire <priyanka.khire@gmail.com>
2018-07-31 20:31:35 -0700

    Added past approach as comment

--- Simplify Path.py
+++ Simplify Path.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #Given an absolute path for a file (Unix-style), simplify it.
 #
 #For example,
@@ -60,3 +61,73 @@ class Approch1(object):
 #Main
 o = Approch1("/...")
 print o.solution()
+
+'''
+=======
+#Simplify Path
+#
+#Given an absolute path for a file (Unix-style), simplify it.
+#
+#For example,
+#path = "/home/", => "/home"
+#path = "/a/./b/../../c/", => "/c"
+#
+#Corner Cases:
+#
+#Did you consider the case where path = "/../"?
+#In this case, you should return "/".
+#Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+#In this case, you should ignore redundant slashes and return "/home/foo".
+class Approach1(object):
+    def __init__(self, string):
+        self.string = string
+        self.stack = []
+
+    #insert input string in stack in reverse order such that top of stack is beginning of string
+    def insertInStack(self):
+        for i in range(len(self.string)-1, -1, -1):
+            self.stack.append(self.string[i])
+
+    def applyRulesToOutput(self, currString, previousString):
+        if (currString == "/"):
+            return previousString, currString
+        #/path/ = /path
+        if(currString[-1] == "/"):
+            currString = currString[:-1]
+        # /path/../ = /
+        if(currString == ".."):
+            return "", ""
+        # /path/./ = /path
+        if(currString == "."):
+            return previousString, "/"
+        return previousString, currString
+            
+
+    def processSolution(self):
+        self.insertInStack()
+        output =  self.stack.pop()
+        previous = ""
+        while self.stack:
+            #pop from stack till next / is found
+            current = ""
+            while (self.stack and self.stack[-1] != "/"):
+                current = current+self.stack.pop()
+            #Add the next / to current
+            current = current +self.stack.pop()
+            previous, current = self.applyRulesToOutput(current, previous)
+            output = output + previous
+            previous = current
+        output = output+previous
+        print output
+
+            
+
+
+
+
+#Main
+        #put print statements and verify why is this wrong
+o = Approach1("/a/./b/../../c/")
+o.processSolution()
+>>>>>>> Stashed changes
+'''
