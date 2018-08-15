2d205bbccbe4cb8db259dba6b9e01d15d89cca53
Priyanka Khire <priyanka.khire@gmail.com>
2018-08-15 12:21:43 -0700

    Complete working recurrssive approch

--- Regular Expression Matching.py
+++ Regular Expression Matching.py
@@ -7,42 +7,92 @@ class Approch1(object):
         self.string = string
         self.pattern = pattern
 
-    def solution(self, string, pattern):
-        print "String is "+string+" pattern is "+pattern
-        if not pattern:
-            print "Pattern empty, returning False"
-            return False
-        if(string == pattern):
-            print "Pattern = string returning True"
+    def combineAll(self, string, pattern):
+        #print string,pattern
+        if pattern == string:
             return True
+        if not pattern:
+            return False
+        if not string:
+            return False
         if(len(pattern) > 1 and pattern[1] == "*"):
-            print "preeceding character is *"
-            #it will match 0 or  any lengths of preceeding characters
-            while(pattern[0] == "." or string[0] == pattern[0]):
-                print pattern[0], string[0]
-                self.solution(string, pattern[2:])
-                string.pop(0)
-                print "string and pattern in while loop ",
-                print string, pattern
-            print "string and pattern out of while loop ",string, pattern
-            #pop the preeciding character
-            #pattern.pop(0)
-            #pop the *
-            #pattern.pop(0)
+            #a star can match 0 to multiple instance's  of preeceeding character in string
+            #here is a python do while
+            while True:
+                if(self.combineAll(string, pattern[2:])):
+                    return True
+                if not string or (string[0] != pattern[0] and pattern[0] != "."):
+                    return False
+                string = string[1:]
         else:
-            print "string in else Part "+string
-            print "pattern in else part "+pattern
-            if(string[0] == pattern[0] or pattern[0] == "."):
-                string.pop(0)
-                pattern.pop(0)
-                print "string and pattern after popping ",string, pattern
-                self.solution(string, pattern)
-                print "in backtracking portion ", string, pattern
+            if string and (pattern[0] == "." or pattern[0] ==string[0]):
+                if(self.combineAll(string[1:], pattern[1:])):
+                    return True
+            else:
+                return False
+
+    def withDot(self, string, pattern):
+        print string,pattern
+        if pattern == string:
+            return True
+        if not pattern:
+            return False
+        if(len(pattern) > 1 and pattern[1] == "*"):
+            #a star can match 0 to multiple instance's  of preeceeding character in string
+            #here is a python do while
+            while True:
+                if(self.withDot(string, pattern[2:])):
+                    return True
+                if(string[0] != pattern[0] and pattern[0] != "."):
+                    return False
+                string = string[1:]
+
+
+    def withoutDot(self, string, pattern):
+        print string,pattern
+        if pattern == string:
+            return True
+        if not pattern:
+            return False
+        if(len(pattern) > 1 and pattern[1] == "*"):
+            #a star can match 0 to multiple instance's  of preeceeding character in string
+            #here is a python do while
+            while True:
+                if(self.withoutDot(string, pattern[2:])):
+                    return True
+                if(string[0] != pattern[0]):
+                    return False
+                string = string[1:]
+            
+                
+    def withoutStar(self, string, pattern):
+        print string, pattern
+        if pattern == string:
+            return True
+        if not pattern:
+            return False
+        #let's simplify the problem
+        #what if there are no *
+        if(len(pattern) > 1 and pattern[1] != "*"):
+            if(pattern[0] == "." or pattern[0] ==string[0]):
+                if(self.withoutStar(string[1:], pattern[1:])):
+                    return True
+            else:
+                return False
         
+
+    def solution(self):
+        print self.withoutStar("aab", "a.b")
+        print self.withoutDot("aab", "c*a*b")
+        print self.withDot("bbbc", ".*")
+        print self.combineAll("bbc", ".*c")
+        print self.combineAll("aa", "a")
+        print self.combineAll("ab", ".*c")
+        print self.combineAll("aaaaaaaaaaaaab","a*a*a*a*a*a*a*a*a*a*c")
                 
         
 
 #Main
-o = Approch1("aab", "c*a*b")
-o.solution("aaa", ".*a*")
+o = Approch1("bbbc", ".*")
+o.solution()
 
