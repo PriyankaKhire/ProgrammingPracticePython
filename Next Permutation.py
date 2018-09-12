fc71a550cd4d866aab6a15dce437da0a33b16ebd
priyankakhire <priyanka.khire@gmail.com>
2018-09-12 14:56:56 -0700

    Added approch 2, fully working

--- Next Permutation.py
+++ Next Permutation.py
@@ -29,6 +29,47 @@ class Approch1(object):
                 smallest = int(''.join(str(e) for e in l))
         print [int(x) for x in str(smallest)]
 
+class Approch2(object):
+    #reverses the numbers from given index to end
+    def reverse(self, nums, index):
+            totalNumsToReverse = (len(nums)-1)-index
+            for i in range(totalNumsToReverse/2):
+                nums[index+1+i], nums[(len(nums)-1)-i] = nums[(len(nums)-1)-i], nums[index+1+i]
+            return nums
+    
+    #finds the first number from back that does not follow ascending order
+    def findFirstNumNotAscending(self, nums, high):
+        for i in range(high, 0, -1):
+            if(nums[i-1] < nums[i]):
+                return i-1
+            
+    #finds position of FIRST number that is GREATER than number at given pos    
+    def findFirstNumGreaterThanPos(self, pos, nums):
+        for i in range(len(nums)-1, pos, -1):
+            if(nums[i] > nums[pos]):
+                return i
+        
+
+    def findNextPrermutation(self, nums, high):
+            pos = self.findFirstNumNotAscending(nums, high)
+            if (pos == None):
+                nums.sort()
+                return nums
+            greaterNumPos = self.findFirstNumGreaterThanPos(pos, nums)
+            #Swap numbers
+            nums[pos], nums[greaterNumPos] = nums[greaterNumPos], nums[pos]
+            #reverse rest of the numbers
+            return self.reverse(nums, pos)
+            
+    def nextPermutation(self, nums):
+        return self.findNextPrermutation(nums, len(nums)-1)
+
 #Main
-o = Approch1()
-o.nextPermutation([1,2,3])
+#o = Approch1()
+#o.nextPermutation([1,2,3])
+
+o2 = Approch2()
+print o2.nextPermutation([1,2,5,4,3,2,1])
+
+o3 = Approch2()
+print o3.nextPermutation([3,2,1])
