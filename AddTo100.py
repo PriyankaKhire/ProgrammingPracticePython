2d2d016e6d2e2d15f928f0ce35efd7022ba050d9
Priyanka Khire <priyanka.khire@gmail.com>
2018-09-20 15:08:51 -0700

    incomplete program

--- AddTo100.py
+++ AddTo100.py
@@ -4,12 +4,13 @@
 #example 1 + 2 + 34 - 5 + 67 - 8 + 9 = 100
 class Approch1(object):
     def __init__(self):
-        self.digits = [1,2,3]
+        self.digits = [1,2,3,4,5,6,7,8,9,0]
         self.operations = ['add', 'sub', 'nothing']
 
     def logic(self, index, number, string, s):
-        if( index == len(self.digits)-1):
-            print string, s
+        if( index >= len(self.digits)-1):
+            if (s == 10):
+                print string, number, s
             return
         for operation in self.operations:
             if(operation == 'add'):
@@ -17,10 +18,10 @@ class Approch1(object):
             elif(operation == 'sub'):
                 self.logic(index+1, self.digits[index+1], string+"-"+str(number), s-self.digits[index])
             elif(operation == 'nothing'):
-                
+                self.logic(index+2, (number*10)+self.digits[index+1], string, s)
                 
     def solution(self):
-        self.logic()
+        self.logic(0,1,"",0)
 
 #Main
 o = Approch1()
