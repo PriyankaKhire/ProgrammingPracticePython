643aafef10755d5aa37d5882e2e0dc8c448c066d
Priyanka Khire <priyanka.khire@gmail.com>
2018-11-26 13:04:56 -0800

    Corrected this approch after thinking alot and then gave up, came back after break and solved in 1 hour, sometimes break is good

--- AddTo100.py
+++ AddTo100.py
@@ -4,36 +4,39 @@
 #example 1 + 2 + 34 - 5 + 67 - 8 + 9 = 100
 class Approch1(object):
     def __init__(self):
-        self.digits = [1,2,3,0]
-        self.operations = ['add', 'sub', 'nothing']
+        self.digits = [1,2,3,4,5,6,7,8,9]
 
-    def logic(self, index, number, array, s):
-        if(index == len(self.digits)-1):
-            print array, s
+    def logic2(self, index, output, s):
+        if(index == len(self.digits)):
+            if(s==100):
+                print output, s
             return
-        #for every index we have 3 operations
-        for operation in self.operations:
-            if(operation == "add"):
-                array.append(number)
-                self.logic(index+1, self.digits[index+1], array, s+number)
-                array.pop()
-            elif(operation == "sub"):
-                array.append(-number)
-                self.logic(index+1, self.digits[index+1], array, s-number)
-                array.pop()
-            else:
-                if array:
-                    top = array.pop()
-                    array.append((top*10)+number)
-                    self.logic(index+1, self.digits[index+1], array, s+((top*10)+number))
-                    array.pop()
-                    array.append(-((top*10)+number))
-                    self.logic(index+1, self.digits[index+1], array, s-((top*10)+number))
-                    array.pop()
-                
+        #add
+        output.append(self.digits[index])
+        self.logic2(index+1, output, s+self.digits[index])
+        output.pop()
+        #subctract
+        output.append(-self.digits[index])
+        self.logic2(index+1, output, s-self.digits[index])
+        output.pop()
+        #multiply
+        if(output):
+            top = output.pop()
+            #*********************************************
+            #this is where I went wrong in previous approches  #*
+            n = 0                                                                       #*
+            if(top > 0):                                                              #*
+                n = (top*10)+self.digits[index]                            #*
+            else:                                                                      #*
+                n = (top*10)-self.digits[index]                           #*
+            #*********************************************
+            output.append(n)                                                
+            self.logic2(index+1, output, (s-top)+n)
+            #output.pop() <- add this line back and see how it changes in memory, and thats why I went wrong previously too. because I put this in for loop
+            #lesson learnt, dont use for loop when you have less instances and when its not actually required.    
                     
     def solution(self):
-        self.logic(0, self.digits[0], [], 0)
+        self.logic2(0, [], 0)
 
 #Main
 o = Approch1()
