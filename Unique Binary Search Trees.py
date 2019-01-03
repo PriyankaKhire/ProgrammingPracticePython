bf6e399bdbd83eeebf8f1e8a5a1b8428615ef082
Priyanka Khire <priyanka.khire@gmail.com>
2019-01-02 16:23:06 -0800

    Leetcode accepted dp solution

--- Unique Binary Search Trees.py
+++ Unique Binary Search Trees.py
@@ -11,10 +11,22 @@ class Solution(object):
     # when 3 is root it has 1,2 on left side and 4 on right side and when 4 is root it has 1,2,3 on left side
     #thus it is dp[3]+(dp[1]*dp[2])+(dp[2]*dp[1])+dp[3] = 5+(1*2)+(2*1)+5 = 14
     #thus the formula becomes:
-    # (dp[n-1]+dp[n-2]*dp[1]+...+(dp[n/2]*dp[n/2 -1]))*2
+    # (dp[n-1]*dp[0]+dp[n-2]*dp[1]+...+(dp[n/2]*dp[n/2 -1]))*2
     def evenN(self,n):
-        halfTotal = self.dp[n-1]
-        i = 1
+        halfTotal = 0
+        for i in range(n/2):
+            halfTotal = halfTotal + (self.dp[i] * self.dp[n-i-1])
+        self.dp[n] = 2*halfTotal
+
+    #similarly the formula for odd n is
+    #dp[n-1]*dp[0] + dp[n-2]*dp[1] ... dp[n+1 /2]*dp[n+1 / 2 - 1]
+    #dont get too confused its same as even n but just add dp[n-1/2]*dp[n-1/2]
+    #if you get too confused by this just see tushar roy's explanation
+    def oddN(self,n):
+        halfTotal = 0
+        for i in range((n-1)/2):
+            halfTotal = halfTotal + (self.dp[i]*self.dp[n-i-1])
+        self.dp[n] = (2*halfTotal)+(self.dp[(n-1)/2]*self.dp[(n-1)/2])
         
         
     def numTrees(self, n):
@@ -23,8 +35,12 @@ class Solution(object):
         self.dp = [0 for i in range(n+1)]
         self.dp[0] = 1
         self.dp[1] = 1
-        if(n%2==0):
-            return self.evenN(n)
+        for i in range(2, n+1):
+            if(i%2 == 0):
+                self.evenN(i)
+            else:
+                self.oddN(i)
+        return self.dp
         
         """
         :type n: int
@@ -32,4 +48,4 @@ class Solution(object):
         """
 #Main
 obj = Solution()
-print obj.numTrees(2)
+print obj.numTrees(6)
