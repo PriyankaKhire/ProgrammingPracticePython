9509ecd062ce0ee74c209b4ee7dbc19ddf9a3e3b
priyankakhire <priyanka.khire@gmail.com>
2019-01-06 12:10:02 -0800

    Added incomplete approch

--- Burst Balloons.py
+++ Burst Balloons.py
@@ -1,6 +1,33 @@
 #Burst Balloons
 #https://leetcode.com/problems/burst-balloons/
 
+class Approch2(object):
+    def __init__(self, array):
+        self.array = array
+        self.matrix = [[0 for col in range(len(array))] for row in range(len(array))]
+
+    def displayMatrix(self):
+        for row in range(len(self.array)):
+            print self.matrix[row]
+
+    def fillMatrix(self):
+        #Filling matrix diagonally
+        colStart = 0
+        row = 0
+        col =  0
+        while(colStart < len(self.array)):
+            self.matrix[row][col] = 1
+            col = col+1
+            row = row+1
+            if(row == len(self.array) or col == len(self.array)):
+                row = 0
+                colStart = colStart+1
+                col = colStart
+
+    def logic(self):
+        self.fillMatrix()
+        self.displayMatrix()
+
 class Approch1(object):
     def __init__(self, array):
         self.array = array
@@ -50,5 +77,8 @@ class Approch1(object):
 
 
 #Main
-obj = Approch1([3,1,5,8])
-obj.logic()
+obj1 = Approch1([3,1,5,8])
+obj1.logic()
+
+#obj2 = Approch2([3,1,5,8])
+#obj2.logic()
