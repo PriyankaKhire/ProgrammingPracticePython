658c6bb15369530209c337754c0bb2ecd8c39af6
Priyanka Khire <priyanka.khire@gmail.com>
2019-02-15 09:22:32 -0800

    DFS backtracking approch, leetcode time limit exceeded

--- Interleaving String.py
+++ Interleaving String.py
@@ -9,18 +9,25 @@ class Approch1(object):
     def fillMatrix(self, s1Index, s2Index, s3Index):
         #Using the dfs approch
         if(s3Index == len(self.s3)):
-            print "true"
-            return True
+            if(s2Index == len(self.s2) and s1Index == len(self.s1)):
+                return True
+            return 
         #checking s1
         if(s1Index < len(self.s1) and self.s1[s1Index] == self.s3[s3Index]):
-            self.fillMatrix(s1Index+1, s2Index, s3Index+1)
+            #print "equal to S1 Index"
+            if(self.fillMatrix(s1Index+1, s2Index, s3Index+1)):
+                return True
         #checking s2
         if(s2Index < len(self.s2) and self.s2[s2Index] == self.s3[s3Index]):
-            self.fillMatrix(s1Index, s2Index+1, s3Index+1)
+            #print "equal to S2 Index"
+            if(self.fillMatrix(s1Index, s2Index+1, s3Index+1)):
+                return True
                 
     def logic(self):
-        self.fillMatrix(0, 0, 0)
+        if self.fillMatrix(0, 0, 0):
+            return True
+        return False
         
 #Main
-obj1 = Approch1("aaa", "bbb", "aa")
-obj1.logic()
+obj1 = Approch1("aaa", "bbb", "aab")
+print obj1.logic()
