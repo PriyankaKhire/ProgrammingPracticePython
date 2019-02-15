7ce9f1edfad8ad841eed7e5921c0a5d3a9bd6431
Priyanka Khire <priyanka.khire@gmail.com>
2019-02-15 10:50:04 -0800

    Added iterative approch, leetcode accepted

--- Ugly Number.py
+++ Ugly Number.py
@@ -21,7 +21,32 @@ class Approch1(object):
         :type num: int
         :rtype: bool
         """
+class Approch2(object):
 
+    def logic(self, num):
+        while num >0:
+            if num == 1:
+                return True
+            if(num%2 == 0):
+                num = num/2
+            elif(num%3 == 0):
+                num = num/3
+            elif(num%5 == 0):
+                num = num/5
+            else:
+                break
+            
+    def isUgly(self, num):
+        if self.logic(num):
+            return True
+        return False
+        """
+        :type num: int
+        :rtype: bool
+        """
 #Main
 obj1 = Approch1()
-obj1.isUgly(8)
+print obj1.isUgly(14)
+
+obj2 = Approch2()
+print obj2.isUgly(14)
