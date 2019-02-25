9eb0f239003ea6ea5272e5ef7645b76c48ad666b
priyankakhire <priyanka.khire@gmail.com>
2019-02-24 20:53:43 -0800

    incomplete, complete it

---
+++ Longest Absolute File Path.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    
+    def logic(self, path):
+        
+        
+    def lengthLongestPath(self, path):
+        """
+        :type input: str
+        :rtype: int
+        """
+#Main
+obj = Solution()
+obj.lengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext")
