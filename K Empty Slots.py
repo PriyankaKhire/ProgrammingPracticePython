f3f132f00170313e6877ca63314964c1b23bc1a4
Priyanka Khire <priyanka.khire@gmail.com>
2019-02-27 11:37:05 -0800

    Added O(n^2) solution leetcode accepted

--- K Empty Slots.py
+++ K Empty Slots.py
@@ -34,6 +34,52 @@ class Bruteforce(object):
         :type k: int
         :rtype: int
         """
+class LookAroundApproch(object):
+    def __init__(self):
+        self.flowerBed = []
+
+    def isValid(self, window):
+        if(window < len(self.flowerBed) and window >= 0):
+            return True
+        return False
+
+    def lookInWindow(self, low, high, k):
+        if(self.flowerBed[low] == True and  self.flowerBed[high] == True):
+            for i in range(low+1, high):
+                if not(self.flowerBed[i] == False):
+                    return False
+            return True
+        return False
+
+    #create a window around i that satisfies the k condition
+    def lookAround(self, slot, k):
+        print self.flowerBed
+        #create a window on left
+        print "left window ", slot-k-1
+        if(self.isValid(slot-k-1) and self.lookInWindow(slot-k-1, slot, k)):
+            return True
+        print "right window ", slot+k+1
+        if(self.isValid(slot+k+1) and self.lookInWindow(slot, slot+k+1, k)):
+            return True
+        return False
+            
+
+    def fillFlowerBed(self, flowers, k):
+        for i in range(len(flowers)):
+            self.flowerBed[flowers[i]-1] = True
+            print "slot = ", flowers[i]-1
+            if(self.lookAround(flowers[i]-1, k)):
+                return i+1
+        return -1
+        
+    def kEmptySlots(self, flowers, k):
+        self.flowerBed = [False for i in range(len(flowers))]
+        print self.fillFlowerBed(flowers, k)
+        """
+        :type flowers: List[int]
+        :type k: int
+        :rtype: int
+        """
 
 #Main
 obj1 = Bruteforce()
@@ -41,3 +87,9 @@ obj1.kEmptySlots([1,3,2], 1)
 
 obj2 = Bruteforce()
 obj2.kEmptySlots([1,2,3], 1)
+
+obj3 = LookAroundApproch()
+obj3.kEmptySlots([1,3,2], 1)
+
+obj4 = LookAroundApproch()
+obj4.kEmptySlots([1,2,3], 1)
