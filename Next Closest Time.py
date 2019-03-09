8c956b4cb133c594522738c0bc4b6557df6e24ba
priyankakhire <priyanka.khire@gmail.com>
2019-03-09 14:36:55 -0800

    haha came up with stupidest solution thats 100% faster than other leet code solutions (leetcode accepted)

--- Next Closest Time.py
+++ Next Closest Time.py
@@ -11,8 +11,8 @@ class Solution(object):
                 self.numbers.append(int(char))
         self.numbers.sort()
 
-    def findNextGreatTime(self, time):
-        return ""
+    def replaceString(self, time, index, string):
+        return time[:index]+string+time[index+1:]
 
     def findGreaterNumber(self, lastNumber):
         for number in self.numbers:
@@ -22,15 +22,27 @@ class Solution(object):
 
     def logic(self,time):
         self.sortNumbers(time)
-        lastNumber = int(time[-1])
-        #now there are 2 cases with last number
+        #now there are few cases with last number
         #1) if we find the a nubmer in sorted numbers to be greater than current last number in time
-        greaterNumber, flag = self.findGreaterNumber(lastNumber)
+        greaterNumber, flag = self.findGreaterNumber(int(time[-1]))
         if(flag):
-            return time[:4]+str(greaterNumber)+time[5:]
-        else:
-            return self.
-            
+            return self.replaceString(time, 4, str(greaterNumber))
+        #2) if we find a number in sorted number to be greater than current second last number in time
+        secondGreaterNumber, flag = self.findGreaterNumber(int(time[-2]))
+        if(flag and secondGreaterNumber < 6):
+            time = self.replaceString(time, 3, str(secondGreaterNumber))
+            return self.replaceString(time, 4, str(self.numbers[0]))
+        #3) if we find a number in sorted numbers to be greater than current third last number in time
+        thirdLastNumber, flag = self.findGreaterNumber(int(time[-4]))
+        if(flag and int(time[0]+str(thirdLastNumber)) < 24 ):
+            time = self.replaceString(time, 1, str(thirdLastNumber))
+            time = self.replaceString(time, 3, str(self.numbers[0]))
+            return self.replaceString(time, 4, str(self.numbers[0]))
+        #4)else replace everything with the smallest number
+        time = self.replaceString(time, 0, str(self.numbers[0]))
+        time = self.replaceString(time, 1, str(self.numbers[0]))
+        time = self.replaceString(time, 3, str(self.numbers[0]))
+        return self.replaceString(time, 4, str(self.numbers[0]))
         
     def nextClosestTime(self, time):
         print self.logic(time)
@@ -40,4 +52,4 @@ class Solution(object):
         """
 #Main
 obj = Solution()
-obj.nextClosestTime("19:39")
+obj.nextClosestTime("23:59")
