57ee0294474edb355a382a9bcb6af3ffc020432b
priyankakhire <priyanka.khire@gmail.com>
2019-05-06 18:59:23 -0700

    Improved solution 2, now working

--- Pour Water.py
+++ Pour Water.py
@@ -70,39 +70,67 @@ class Solution(object):
         :rtype: List[int]
         """
 class Solution2(object):
-    def __init__(self):
-        self.left = {}
-        self.right = {}
-
-    def indicesSort(self, index, heights):
-        #add indices to left of our index to hash table
-        for i in range(index-1, -1, -1):
-            #add to left
-            if not heights[i] in self.left:
-                self.left[heights[i]] = [i]
+    #inspired by http://zxi.mytechroad.com/blog/simulation/leetcode-755-pour-water/
+    def findLeftHole(self, heights, index):
+        print "Finding left hole"
+        hole = index-1
+        while(hole >= 0):
+            print "Currently at hole index ", hole," with height ", heights[hole]
+            if(heights[hole] <= heights[index] and heights[hole-1] <= heights[hole]):
+                print "Finding next hole"
+                hole = hole - 1
+            elif(heights[hole] <= heights[index] and heights[hole-1] > heights[hole]):
+                print "breaking"
+                break
             else:
-                self.left[heights[i]].append(i)
-        #add indices to right of our index to hash table
-        for i in range(index+1, len(heights)):
-            #add to right
-            if not heights[i] in self.right:
-                self.right[heights[i]] = [i]
+                break
+        if(heights[hole] < heights[index]):
+            print "Returning hole ", hole
+            return hole
+        return -1
+
+    def findRightHole(self, heights, index):
+        print "Finding right hole"
+        hole = index+1
+        while(hole < len(heights)-1):
+            print "Currently at hole index ", hole," with height ", heights[hole]
+            if(heights[hole] <= heights[index] and heights[hole+1] <= heights[hole]):
+                print "Finding next hole"
+                hole = hole + 1
+            elif(heights[hole] <= heights[index] and heights[hole+1] > heights[hole]):
+                print "breaking"
+                break
             else:
-                self.right[heights[i]].append(i)
-        print self.left, self.right
-
-    def logic(self, heights, waterUnits, index):
-        while(waterUnits > 0):
-            waterUnits = waterUnits - 1
-            #find if any height lesser than our current height
-            if(sorted(self.left)[0] < heights[index]):
-                for i in range(len(sorted(self.left))):
-                    
-            elif(sorted(self.right)[0] < heights[index]):
-            
-
+                break
+        if(heights[hole] < heights[index]):
+            print "Returning hole ", hole
+            return hole
+        return -1
+    
     def pourWater(self, heights, V, K):
-        self.indicesSort(K, heights)
+        for waterDrop in range(V):
+            print heights
+            print "Pouring water droplet number ", waterDrop
+            #find left hole
+            leftHole = self.findLeftHole(heights, K)
+            rightHole = self.findRightHole(heights, K)
+            print "Left hole is ", leftHole, " right Hole is ", rightHole
+            if(leftHole != -1 and rightHole != -1):
+                print "we have found 2 holes less than current index hole"
+                if(heights[leftHole] <= heights[rightHole]):
+                    heights[leftHole] = heights[leftHole] + 1
+                else:
+                    heights[rightHole] = heights[rightHole] + 1
+            elif((leftHole == -1) and rightHole != -1):
+                print "Left hole ain't there but right is"
+                heights[rightHole] = heights[rightHole] + 1
+            elif((rightHole == -1) and leftHole != -1):
+                print "Right hole ain't there but left is "
+                heights[leftHole] = heights[leftHole] + 1
+            else:
+                print "Filling current index"
+                heights[K] = heights[K] + 1
+        print heights
         
 
 #Main
@@ -113,6 +141,10 @@ obj12.pourWater([2,1,1,2,1,2,2], 4, 3)
 print "\n"
 #obj2 = Solution()
 #obj2.pourWater([1,2,3,4], 2, 2)
+obj22 = Solution2()
+obj22.pourWater([1,2,3,4], 2, 2)
 print "\n"
 #obj3 = Solution()
 #obj3.pourWater([3,1,3], 5, 1)
+obj32 = Solution2()
+obj32.pourWater([3,1,3], 5, 1)
