ab58b6de49c3714e6f9dbcdbc9aeca93cdd36b11
Priyanka Khire <priyanka.khire@gmail.com>
2019-05-22 14:48:55 -0700

    Correct solution with leetcode time limit exceeded

--- Employee Free Time.py
+++ Employee Free Time.py
@@ -1,6 +1,6 @@
 #Employee Free Time
 #https://leetcode.com/problems/employee-free-time/
-class Solution(object):
+class Solution1(object):
     def __init__(self):
         self.intervals = None
     
@@ -44,12 +44,60 @@ class Solution(object):
         :type schedule: List[List[List[int]]]
         :rtype: List[List[int]]
         """
+
+class Solution2(object):
+    def flattenAndSortIntervals(self,schedule):
+        output = []
+        for employeeTime in schedule:
+            for interval in schedule:
+                for time in interval:
+                    output.append(time)
+        output.sort()
+        return output
+
+    def combineIntervals(self, intervals):
+        output = []
+        while(intervals):
+            i = 1
+            while(i < len(intervals) and intervals[0][0] <= intervals[i][0] and intervals[0][1] >= intervals[i][0]):
+                if(intervals[0][1] < intervals[i][1]):
+                    intervals[0][1] = intervals[i][1]
+                i = i + 1
+            output.append(intervals[0])
+            #pop the intervals
+            j = 0
+            while(intervals and j<i):
+                intervals.pop(0)
+                j = j+1
+        return output
+
+    def findGap(self, intervals):
+        output = []
+        for i in range(1, len(intervals)):
+            if(intervals[i-1][1] < intervals[i][0]):
+                output.append([intervals[i-1][1], intervals[i][0]])
+        return output
+
+    def employeeFreeTime(self, schedule):
+        intervals = self.flattenAndSortIntervals(schedule)
+        combinedIntervals = self.combineIntervals(intervals)
+        return self.findGap(combinedIntervals)
+        """
+        :type schedule: List[List[List[int]]]
+        :rtype: List[List[int]]
+        """
 #Main
-obj1 = Solution()
+obj1 = Solution1()
+print obj1.employeeFreeTime([[[1,2],[5,6]],[[1,3]],[[4,10]]])
+obj1 = Solution2()
 print obj1.employeeFreeTime([[[1,2],[5,6]],[[1,3]],[[4,10]]])
 
-obj2 = Solution()
+obj2 = Solution1()
+print obj2.employeeFreeTime([[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]])
+obj2 = Solution2()
 print obj2.employeeFreeTime([[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]])
 
-obj3 = Solution()
+obj3 = Solution1()
+print obj3.employeeFreeTime([[[45,56],[89,96]],[[5,21],[57,74]]])
+obj3 = Solution2()
 print obj3.employeeFreeTime([[[45,56],[89,96]],[[5,21],[57,74]]])
