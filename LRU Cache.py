d09e94fd59f13fce5414fecefd1ca0020b373636
priyankakhire <priyanka.khire@gmail.com>
2019-06-16 14:15:10 -0700

    This solution does not work if keys are not unique

--- LRU Cache.py
+++ LRU Cache.py
@@ -15,14 +15,51 @@ class DLLStack(object):
     def createNode(self, key, value):
         return DLLNode(key, value)
 
-    def addFromTop(self, key, value):
-        node = self.createNode(key, value)
+    def removeNodeAddToTop(self, node):
+        #if node is top node do nothing
+        if( node == self.top):
+            return
+        #if node is bottom node
+        if(node == self.bottom):
+            self.deleteFromBottom()
+            self.addNodeFromTop(node)
+            return
+        #if node is in middle
+        self.deleteFromMid(node)
+        self.addNodeFromTop(node)
+
+    def addNodeFromTop(self, node):
         if(self.top != None):
             node.previous = self.top
             self.top.next = node
         if(self.top == None):
             self.bottom = node
         self.top = node
+
+    def addFromTop(self, key, value):
+        node = self.createNode(key, value)
+        self.addNodeFromTop(node)
+
+    def deleteFromMid(self, node):
+        #if node is the only one
+        if(node == self.top and node == self.bottom):
+            self.top = None
+            self.bottom = None
+            return
+        #if node is top
+        if(node == self.top):
+            self.top = self.top.previous
+            self.top.next = None
+            return
+        #if node is bottom
+        if(node == self.bottom):
+            self.deleteFromBottom()
+            return
+        #if node is mid
+        previousNode = node.previous
+        nextNode = node.next
+        nextNode.previous = previousNode
+        previousNode.next = nextNode
 
     def deleteFromBottom(self):
         if(self.bottom == None):
@@ -39,7 +76,7 @@ class DLLStack(object):
     def display(self):
         ptr = self.top
         while(ptr != None):
-            print ptr.data,
+            print [ptr.key, ptr.value],
             ptr = ptr.previous
         print ""
 
@@ -48,6 +85,7 @@ class LRUCache(object):
 
     def __init__(self, capacity):
         self.capacity = capacity
+        self.currentCapacity = 0
         self.index = {}
         self.dllQ = DLLStack()
         """
@@ -56,6 +94,13 @@ class LRUCache(object):
         
 
     def get(self, key):
+        if not(key in self.index):
+            return -1
+        node = self.index[key]
+        self.dllQ.removeNodeAddToTop(node)
+        self.dllQ.display()
+        print self.index
+        return node.value
         """
         :type key: int
         :rtype: int
@@ -63,6 +108,16 @@ class LRUCache(object):
         
 
     def put(self, key, value):
+        if(self.currentCapacity == self.capacity):
+            #delete from queue and index
+            k = self.dllQ.bottom.key
+            del self.index[k]
+            self.dllQ.deleteFromBottom()
+            self.currentCapacity = self.currentCapacity - 1
+        self.dllQ.addFromTop(key, value)
+        self.index[key] = self.dllQ.top
+        self.currentCapacity = self.currentCapacity + 1
+        self.dllQ.display()
         """
         :type key: int
         :type value: int
@@ -76,4 +131,8 @@ class LRUCache(object):
 # param_1 = obj.get(key)
 # obj.put(key,value)
 #Main
-#obj = LRUCache()
+obj = LRUCache(2)
+obj.put(2,1)
+obj.put(2,2)
+print obj.get(2)
+#This solution does not work if keys are not unique
