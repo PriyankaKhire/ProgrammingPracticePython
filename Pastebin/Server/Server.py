cd6d91f409861e96d011d556814916fbfd5e8376
priyankakhire <priyanka.khire@gmail.com>
2019-06-24 18:35:22 -0700

    Basic pastebin server

---
+++ Pastebin/Server/Server.py
@@ -0,0 +1,53 @@
+#Server
+import socket, pickle
+import sys
+from PasteClass import Paste
+class Server(object):
+    def __init__(self):
+        self.id = 1
+        self.port = 8
+
+    def server(self):
+        s = socket.socket()
+        s.bind(('', self.port))
+        s.listen(5)
+        print "Started server"
+        return s
+
+    def approch1(self, pasteObj):
+        #Send data to Approch1 server
+        port = 1
+        s = socket.socket()
+        s.connect(('127.0.0.1', port))
+        object_string = pickle.dumps(pasteObj)
+        s.send(object_string)
+        print s.recv(1024)
+        s.close()
+
+    def processData(self, text, fileName, folderName, userId):
+        #We first create Paste object.
+        pasteObj = Paste(userId, text, fileName, folderName)
+        self.approch1(pasteObj)
+        
+
+    def run(self):
+        #Start the server
+        s = self.server()
+        while True:
+            c, addr = s.accept()
+            userId = c.recv(1024)
+            c.send('Server acknowledges receipt of user id')
+            text = c.recv(1024)
+            c.send('Server acknowledges receipt of text')
+            print "Got text from user ", userId, " the text is \n ", text
+            fileName = c.recv(1024)
+            c.send('Server acknowledges receipt of file name')
+            folderName = c.recv(1024)
+            c.send('Server acknowledges receipt of folder name')
+            print "File Name ", fileName, " Folder Name ", folderName
+            c.close()
+            self.processData(text, fileName, folderName, userId)
+
+#Main
+obj = Server()
+obj.run()
