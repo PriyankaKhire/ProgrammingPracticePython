3f2ab99a377f9c3873c0e1d0d37c42e451f7bb49
priyankakhire <priyanka.khire@gmail.com>
2019-06-27 10:36:33 -0700

    Added Client data

--- Load Balancing/Config.py
+++ Load Balancing/Config.py
@@ -3,7 +3,7 @@ class ServerConfig(object):
     def __init__(self):
         #key is server name and value is port number
         self.port = {
-            'Server1':1001,
+            'Server1':1041,
             'Server2':2002,
             'Server3':3003,
             'Server4':4004,
@@ -20,6 +20,23 @@ class ApprochConfig(object):
     def __init__(self):
         #key is Approch Server name and value is port number
         self.port = {
-            'Approch1':101,
+            'Approch1':94,
             'Approch2':200
+            }
+        
+#Config file specifying the data for client file
+class ClientData(object):
+    def __init__(self):
+        self.data = {
+            0:'aaa',
+            1:'bbb',
+            2:'ccc',
+            3:'ddd',
+            4:'eee',
+            5:'fff',
+            6:'ggg',
+            7:'hhh',
+            8:'iii',
+            9:'jjj',
+            10:'kkk'
             }
