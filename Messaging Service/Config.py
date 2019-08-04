ed491fd8e47441d1f8fb7887f37d403c7dc09d94
priyankakhire <priyanka.khire@gmail.com>
2019-08-03 17:17:44 -0700

    Modified config file to add Chat server with message storing

--- Messaging Service/Config.py
+++ Messaging Service/Config.py
@@ -2,5 +2,8 @@
 class PortConfig(object):
     def __init__(self):
         self.port = {
-            'SimpleChatServer' : 5016
+            'SimpleChatServer' : 5016,
+            'MessageStoreChatServer' : 6066,
+            'StoreMessagesDatabase' : 7505,
+            'GetMessagesDatabase' : 8514
             }
