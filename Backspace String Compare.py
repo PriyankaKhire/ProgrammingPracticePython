43e815d1f738e97ebbd89282ee1ccf854204831d
priyankakhire <priyanka.khire@gmail.com>
2019-08-09 19:48:02 -0700

    added leetcode suggested method, leetcode accepted

--- Backspace String Compare.py
+++ Backspace String Compare.py
@@ -18,10 +18,28 @@ class Solution(object):
         :type T: str
         :rtype: bool
         """
+class LeetcodeSuggested(object):
+    def getFinalString(self, string):
+        skipCount = 0
+        stng = ""
+        for char in reversed(string):
+            if(char == '#'):
+                skipCount = skipCount+1
+            else:
+                if(skipCount > 0):
+                    skipCount = skipCount -1
+                else:
+                    stng = stng + char
+        return stng[::-1]
+
+    def backspaceCompare(self, S, T):
+        return self.getFinalString(S) == self.getFinalString(T) 
 
 # Main
 obj = Solution()
+obj = LeetcodeSuggested()
 print obj.backspaceCompare("ab#c", "ab#c")
 
 obj = Solution()
+obj = LeetcodeSuggested()
 print obj.backspaceCompare("a#c", "b")
