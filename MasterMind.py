786a18a9603b5122a096b3e1e99935b284b80fe5
priyankakhire <priyanka.khire@gmail.com>
2019-08-28 19:35:16 -0700

    improvement2, we use dummy color and find place of color, inspired by game of hangman

--- MasterMind.py
+++ MasterMind.py
@@ -91,6 +91,7 @@ class Improvement1(object):
         self.allCombinationsOfColorsInSeq(colorsInSequence, self.numberOfColorsInSequence, [])
 
 # Here I make use of dummy color -> 'd'
+# this is a much faster algo and will guess in constant time with respect to colors present in sequence
 class Improvement2(object):
     def __init__(self, master):
         self.master = master
@@ -98,14 +99,41 @@ class Improvement2(object):
         self.numberOfColorsInSequence = len(master.secretSequence)
         self.guessSeq = ['d' for i in range(self.numberOfColorsInSequence)]
 
+    def findCorrectPos(self, color, numberOfPositions, output, index):
+        if(numberOfPositions == 0):
+            score = self.master.guess(output[:])
+            # here we make use of numberOfColorsInDifferentPlaceButSameColor
+            # every time we add a color to its correct pos, the score of the self.guessSeq goes up.
+            # we are testing different colors in their positions.
+            # if there is 1 color in its correct postion and other color is in secret sequence but in its incorrect pos
+            # the numberOfColorsInDifferentPlaceButSameColor variable will report that
+            # if there are 2 colors in their correct pos then numberOfColorsInDifferentPlaceButSameColor = 0
+            if(score[1] == 0):
+                self.guessSeq = output[:]
+                return True
+            return
+        for i in range(index, len(output)):
+            # only replace color where there is dummy color.
+            if(output[i] == 'd'):                
+                output[i] = color
+                if(self.findCorrectPos(color, numberOfPositions-1,  output, i+1)):
+                    # this if condition is to stop recurrssion.
+                    return True
+                # backtrack.
+                output[i] = 'd'
+
     def findPosOfColor(self):
         for color in self.colors:
             score = self.master.guess([color for i in range(self.numberOfColorsInSequence)])
             if(score[0] > 0):
                 print "Color",color,"present in sequence in",score[0],"positions"
+                self.findCorrectPos(color, score[0], self.guessSeq[:], 0)
+                print "Correct position of the color is", self.guessSeq
 
     def run(self):
         self.findPosOfColor()
+        print "We guess",self.guessSeq
+         
 # Main
 m = MasterMind(3, 4)
 #brute = BruteForce(m)
